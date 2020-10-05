962888f27d033f363b07744e77b0daccaa3d351b
Erik Zscheile <zseri.devel@ytrizja.de>
2020-10-05 23:48:29 +0200

    API BREAK (unnamable type): replace poll_state with a wrapper around poll_fn

--- src/lib.rs
+++ src/lib.rs
@@ -58,11 +58,15 @@ where
 /// assert_eq!(poll_state(7, f).await, 8);
 /// # })
 /// ```
-pub fn poll_state<F, S, T>(state: S, fun: F) -> PollState<F, S>
+#[inline]
+pub fn poll_state<F, S, T>(
+    mut state: S,
+    mut fun: F,
+) -> PollFn<impl FnMut(&mut Context<'_>) -> Poll<T>>
 where
     F: FnMut(&mut S, &mut Context<'_>) -> Poll<T>,
 {
-    PollState { state, fun }
+    poll_fn(move |ctx| fun(&mut state, ctx))
 }
 
 /// Future for the [`poll_fn()`] function.
@@ -85,30 +89,6 @@ where
     fn poll(self: Pin<&mut Self>, ctx: &mut Context<'_>) -> Poll<T> {
         let this = unsafe { self.get_unchecked_mut() };
         (this.inner)(ctx)
-    }
-}
-
-/// Future for the [`poll_state()`] function.
-#[must_use = "futures do nothing unless you `.await` or poll them"]
-pub struct PollState<F, S> {
-    fun: F,
-    state: S,
-}
-
-impl<F, S> fmt::Debug for PollState<F, S> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("PollState").finish()
-    }
-}
-
-impl<F, S, T> Future for PollState<F, S>
-where
-    F: FnMut(&mut S, &mut Context<'_>) -> Poll<T>,
-{
-    type Output = T;
-    fn poll(self: Pin<&mut Self>, ctx: &mut Context<'_>) -> Poll<T> {
-        let this = unsafe { Pin::get_unchecked_mut(self) };
-        (this.fun)(&mut this.state, ctx)
     }
 }
 
