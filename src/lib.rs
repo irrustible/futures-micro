9aaf0e9fee5c27f8275100e63669fcbac923feb0
Erik Zscheile <zseri.devel@ytrizja.de>
2020-10-10 17:45:27 +0200

    make ready! impl more compact

--- src/lib.rs
+++ src/lib.rs
@@ -492,7 +492,7 @@ macro_rules! ready {
     ($e:expr $(,)?) => {
         match $e {
             core::task::Poll::Ready(t) => t,
-            core::task::Poll::Pending => return core::task::Poll::Pending,
+            t @ core::task::Poll::Pending => return t,
         }
     };
 }
